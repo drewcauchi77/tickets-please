9bbf915b3d44189412ffbe8b7c0c149db7106909
Andrew Cauchi <drewcauchi@gmail.com>
2025-08-03 16:21:10 +0200

    token auth

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
---
+++ routes/api_v1.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Http\Controllers\Api\V1\TicketController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
