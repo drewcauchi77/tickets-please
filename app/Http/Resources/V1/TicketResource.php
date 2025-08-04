7bdb2e84605cbcc3603e25c24682cf4404a9436c
Andrew Cauchi <drewcauchi@gmail.com>
2025-08-04 21:02:37 +0200

    nested resources

--- app/Http/Resources/V1/TicketResource.php
+++ app/Http/Resources/V1/TicketResource.php
@@ -35,8 +35,8 @@ class TicketResource extends JsonResource
                         'id' => $this->user_id
                     ],
                     'links' => [
-                        'self' => route('users.show', [
-                            'user' => $this->user_id
+                        'self' => route('authors.show', [
+                            'author' => $this->user_id
                         ])
                     ]
                 ]
