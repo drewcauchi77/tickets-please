1110e47da07518781d828af9d24a650b4f2e3b87
Andrew Cauchi <drewcauchi@gmail.com>
2025-08-05 21:08:57 +0200

    replace resource

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -5,6 +5,14 @@ use App\Http\Controllers\Api\V1\AuthorsController;
 use App\Http\Controllers\Api\V1\AuthorTicketsController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
-Route::middleware('auth:sanctum')->apiResource('authors', AuthorsController::class);
-Route::middleware('auth:sanctum')->apiResource('authors.tickets', AuthorTicketsController::class);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('tickets', TicketController::class)->except('update');
+    Route::put('tickets/{ticket}', [TicketController::class, 'replace'])->name('tickets.replace');
+
+    Route::apiResource('authors', AuthorsController::class);
+
+    Route::apiResource('authors.tickets', AuthorTicketsController::class)->except('update');
+    Route::put('authors/{author}/tickets/{ticket}', [AuthorTicketsController::class, 'replace'])->name('author.tickets.replace');
+});
